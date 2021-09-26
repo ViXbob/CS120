99b465e0d73096bbea444bd12889d06d4c59d18b
ViXbob <vixbob@outlook.com>
2021-09-27 00:24:48 +0800

    fix: can recode within a specific time automatically complete: project1/part1/check2

--- cs140-project1/src/task1_ck1.rs
+++ cs140-project1/src/task1_ck1.rs
@@ -1,74 +1,81 @@
-use cs140_buffer::ring_buffer::RingBuffer;
-use cs140_common::device::InputDevice;
-use cs140_common::buffer::Buffer;
-use std::sync::{Arc, Mutex};
-use std::io::BufWriter;
-use std::fs::File;
-use cpal::{Device, SampleFormat, StreamConfig, StreamError};
-use std::ops::Deref;
+// use cs140_buffer::ring_buffer::RingBuffer;
+// use cs140_common::device::InputDevice;
+// use cs140_common::buffer::Buffer;
+// use std::sync::{Arc, Mutex};
+// use std::io::BufWriter;
+// use std::fs::File;
+// use cpal::{Device, SampleFormat, StreamConfig, StreamError};
+// use std::ops::Deref;
+//
+// fn main() -> Result<(), anyhow::Error> {
+//     let buffer : RingBuffer<f32, 100001, false> = RingBuffer::new();
+//     let buffer_ptr = Arc::new(buffer);
+//     let (input, input_config) = InputDevice::new(buffer_ptr.clone());
+//     let input = std::thread::spawn(move || input.listen());
+//
+//     const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
+//
+//     let spec = wav_spec_from_config(&input_config);
+//     // println!("{:?}", spec);
+//
+//     let writer = hound::WavWriter::create(PATH, spec)?;
+//     let writer = Arc::new(Mutex::new(Some(writer)));
+//
+//     let record_time = 10;
+//     let segment_count = 100;
+//     let segment_len = input_config.1.sample_rate.0 / segment_count;
+//
+//     // println!("{}, {}, {}", segment_len, segment_count, writer.try_lock().unwrap().as_ref().unwrap().spec().channels);
+//
+//     for _ in 0..record_time * segment_count {
+//         buffer_ptr.pop_by_ref(segment_len as usize,
+//                               |data| write_input_data::<f32, f32>(data, &writer)
+//         );
+//     }
+//     writer.lock().unwrap().take().unwrap().finalize()?;
+//     input.join();
+//     Ok(())
+// }
+//
+// fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
+//     match format {
+//         cpal::SampleFormat::U16 => hound::SampleFormat::Int,
+//         cpal::SampleFormat::I16 => hound::SampleFormat::Int,
+//         cpal::SampleFormat::F32 => hound::SampleFormat::Float,
+//     }
+// }
+//
+// fn wav_spec_from_config(input: &(Device, StreamConfig, SampleFormat)) -> hound::WavSpec {
+//     hound::WavSpec {
+//         channels: input.1.channels as _,
+//         sample_rate: input.1.sample_rate.0 as _,
+//         bits_per_sample: (input.2.sample_size() * 8) as _,
+//         sample_format: sample_format(input.2),
+//     }
+// }
+//
+// type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
+//
+// fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
+//     where
+//         T: cpal::Sample,
+//         U: cpal::Sample + hound::Sample,
+// {
+//     if let Ok(mut guard) = writer.try_lock() {
+//         if let Some(writer) = guard.as_mut() {
+//             for &sample in input.iter() {
+//                 let sample: U = cpal::Sample::from(&sample);
+//                 for _i in 0..writer.spec().channels {
+//                     writer.write_sample(sample).ok();
+//                 }
+//             }
+//         }
+//     }
+// }
 
-fn main() -> Result<(), anyhow::Error> {
-    let buffer : RingBuffer<f32, 100001, false> = RingBuffer::new();
-    let buffer_ptr = Arc::new(buffer);
-    let (input, input_config) = InputDevice::new(buffer_ptr.clone());
-    let input = std::thread::spawn(move || input.listen());
-
+fn main() {
     const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
-
-    let spec = wav_spec_from_config(&input_config);
-    // println!("{:?}", spec);
-
-    let writer = hound::WavWriter::create(PATH, spec)?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
-
-    let record_time = 10;
-    let segment_count = 100;
-    let segment_len = input_config.1.sample_rate.0 / segment_count;
-
-    // println!("{}, {}, {}", segment_len, segment_count, writer.try_lock().unwrap().as_ref().unwrap().spec().channels);
-
-    for _ in 0..record_time * segment_count {
-        buffer_ptr.pop_by_ref(segment_len as usize,
-                              |data| write_input_data::<f32, f32>(data, &writer)
-        );
-    }
-    writer.lock().unwrap().take().unwrap().finalize()?;
-    input.join();
-    Ok(())
-}
-
-fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
-    match format {
-        cpal::SampleFormat::U16 => hound::SampleFormat::Int,
-        cpal::SampleFormat::I16 => hound::SampleFormat::Int,
-        cpal::SampleFormat::F32 => hound::SampleFormat::Float,
-    }
-}
-
-fn wav_spec_from_config(input: &(Device, StreamConfig, SampleFormat)) -> hound::WavSpec {
-    hound::WavSpec {
-        channels: input.1.channels as _,
-        sample_rate: input.1.sample_rate.0 as _,
-        bits_per_sample: (input.2.sample_size() * 8) as _,
-        sample_format: sample_format(input.2),
-    }
-}
-
-type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
-
-fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
-    where
-        T: cpal::Sample,
-        U: cpal::Sample + hound::Sample,
-{
-    if let Ok(mut guard) = writer.try_lock() {
-        if let Some(writer) = guard.as_mut() {
-            for &sample in input.iter() {
-                let sample: U = cpal::Sample::from(&sample);
-                for _i in 0..writer.spec().channels {
-                    writer.write_sample(sample).ok();
-                }
-            }
-        }
-    }
+    let recoder = std::thread::spawn(|| cs140_project1::record_wav::recode(PATH, 10));
+    std::thread::sleep(std::time::Duration::from_secs(15));
+    drop(recoder);
 }
---
+++ cs140-project1/src/task1_ck2.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
+fn main() {
+    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/music.mp3");
+    println!("{}", PATH);
+// Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+// Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open(PATH).unwrap());
+// Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+// Play the sound directly on the device
+    stream_handle.play_raw(source.convert_samples());
+
+// The sound plays in a separate audio thread,
+// so we need to keep the main thread alive while it's playing.
+
+    const PATH1: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded1.wav");
+    let recoder = std::thread::spawn(|| cs140_project1::record_wav::recode(PATH1, 10));
+    std::thread::sleep(std::time::Duration::from_secs(15));
+    drop(recoder);
+}
