9b69bdfcc68be9f618fe4f4b519235d01ded29a5
spedoske <leomundspedoske@gmail.com>
2021-10-13 20:06:54 +0800

    add redundancy

--- cs140-project1/src/task3_ck1_client.rs
+++ cs140-project1/src/task3_ck1_client.rs
@@ -2,13 +2,15 @@ use cs140_network::encoding::HandlePackage;
 use cs140_network::ip::{IPLayer, IPPackage};
 use cs140_network::physical::{PhysicalLayer, PhysicalPackage};
 use cs140_network::redundancy::{RedundancyLayer, RedundancyPackage};
+use cs140_project1::erase_redundancy;
+use reed_solomon_erasure::galois_8::{ReedSolomon};
 
 fn generate_random_data() -> Vec<u8> {
     use rand::prelude::*;
     use rand_pcg::Pcg64;
 
     let mut rng = Pcg64::seed_from_u64(2);
-    return (0..2500).map(|_| rng.gen()).collect();
+    return (0..1250).map(|_| rng.gen()).collect();
 }
 
 fn main() {
@@ -16,8 +18,27 @@ fn main() {
     let physical_layer = PhysicalLayer::new_receive_only(&[4000.0, 5000.0], 500);
     let redundancy_layer = RedundancyLayer::new(physical_layer);
     let mut ip_layer = IPLayer::new(redundancy_layer);
-    let mut index = 0;
-    let data: IPPackage = ip_layer.receive();
+    let mut data: Vec<Option<Vec<u8>>> = Vec::new();
+    let data_shard_count = 6;
+    let parity_shard_count = 6;
+    loop {
+        let package: IPPackage = ip_layer.receive();
+        if package.data[0] >= data_shard_count + parity_shard_count{
+            drop(package)
+        }else{
+            while data.len() < package.data[0] as usize{
+                data.push(None);
+            }
+            data.push(Some(package.data));
+        }
+        if data.len() as u8 > data_shard_count + parity_shard_count / 2 {
+            while (data.len() as u8) < (data_shard_count + parity_shard_count) {
+                data.push(None);
+            }
+            break;
+        }
+    }
+    let data = erase_redundancy(data, ReedSolomon::new(data_shard_count as usize, parity_shard_count as usize).unwrap(), 10000).unwrap();
     assert_eq!(data.data, generate_random_data())
     // loop{
     //     let data:RedundancyPackage = ip_layer.receive();
--- cs140-project1/src/task3_ck1_server.rs
+++ cs140-project1/src/task3_ck1_server.rs
@@ -3,24 +3,26 @@ use cs140_network::ip::{IPLayer, IPPackage};
 use cs140_network::physical::PhysicalLayer;
 use cs140_network::redundancy::{RedundancyLayer, RedundancyPackage};
 use rand::Rng;
+use cs140_project1::make_redundancy;
 
 fn generate_random_data() -> Vec<u8> {
     use rand::prelude::*;
     use rand_pcg::Pcg64;
 
     let mut rng = Pcg64::seed_from_u64(2);
-    return (0..2500).map(|_| rng.gen()).collect();
+    return (0..1250).map(|_| rng.gen()).collect();
 }
 
 fn main() {
     let data = generate_random_data();
-    for (index, data) in data.iter().enumerate() {
-        println!("{},{}", index, data)
-    }
-    let data = generate_random_data();
+    let padding = 497;
+    let (packages,r) = make_redundancy(data,padding,1.0);
+    println!("data_shard_count: {}, parity_shard_count: {}",r.data_shard_count(),r.parity_shard_count());
     let physical_layer = PhysicalLayer::new_send_only(&[4000.0, 5000.0], 500);
     let redundancy_layer = RedundancyLayer::new(physical_layer);
     let mut ip_layer = IPLayer::new(redundancy_layer);
-    ip_layer.send(IPPackage::new(data));
+    for p in packages{
+        ip_layer.send(IPPackage::new(p));
+    }
     std::thread::park();
 }
