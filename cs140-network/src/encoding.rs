4073e8635465de07f18c62b5743e82e48a24e826
ViXbob <vixbob@outlook.com>
2021-11-25 15:12:02 +0800

    4b5b + nrzi encode

--- cs140-network/src/encoding.rs
+++ cs140-network/src/encoding.rs
@@ -13,6 +13,14 @@ pub trait HandlePackage<Package: NetworkPackage> {
     async fn receive(&mut self) -> Package;
 }
 
+pub fn encode_4b5b(data: &BitStore) -> BitStore {
+    const TABLE: &'static [u8] =  &[0b11110u8, 0b01001u8, 0b10100u8, 0b10101u8,
+        0b01010u8, 0b01011u8, 0b01110u8, 0b01111u8,
+        0b10010u8, 0b10011u8, 0b10110u8, 0b10111u8,
+        0b11010u8, 0b11011u8, 0b11100u8, 0b11101u8];
+
+}
+
 #[cfg(test)]
 mod test {
     use crate::encoding::BitStore;
@@ -29,6 +37,17 @@ mod test {
             for (i, bit) in bits.iter().enumerate() {
                 println!("{}, {}", i, bit);
             }
+        }
+    }
+
+    #[test]
+    fn test_4b5b() {
+        const TABLE: &'static [u8] =  &[0b11110u8, 0b01001u8, 0b10100u8, 0b10101u8,
+                                        0b01010u8, 0b01011u8, 0b01110u8, 0b01111u8,
+                                        0b10010u8, 0b10011u8, 0b10110u8, 0b10111u8,
+                                        0b11010u8, 0b11011u8, 0b11100u8, 0b11101u8];
+        for code in TABLE {
+            println!("{}", code);
         }
     }
 }
