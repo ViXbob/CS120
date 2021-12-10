ed73e717ed847b40e1f85a7b673f40e2d294b0a3
ViXbob <vixbob@outlook.com>
2021-12-11 03:31:10 +0800

    add audio_ping

---
+++ cs140-project3/src/ping.rs
@@ -0,0 +1,27 @@
+use std::net::{IpAddr, SocketAddr, SocketAddrV4};
+use std::str::FromStr;
+use log::trace;
+use cs140_network::ip::{IPLayer, IPPackage};
+use cs140_network::physical::PhysicalLayer;
+use cs140_network::redundancy::RedundancyLayer;
+use cs140_util::icmp::AudioPinger;
+
+const PING_COUNT: usize = 20;
+
+
+#[tokio::main]
+async unsafe fn main() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.format_timestamp_millis().init();
+    let layer = PhysicalLayer::new(16, 64);
+    let layer = RedundancyLayer::new(layer);
+    let mut layer = IPLayer::new(layer);
+
+    let mut pinger = AudioPinger::new(layer, 0x0002);
+
+    for _ in 0..PING_COUNT {
+        pinger.ping_once(IpAddr::from_str("220.181.38.148").unwrap()).await;
+    }
+
+    std::thread::park();
+}
--- cs140-util/src/icmp.rs
+++ cs140-util/src/icmp.rs
@@ -1,9 +1,10 @@
 use std::mem::MaybeUninit;
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 use std::time::Instant;
-use std::net::{AddrParseError, IpAddr};
+use std::net::{AddrParseError, IpAddr, SocketAddrV4};
 use std::net::SocketAddr;
 use std::str::FromStr;
+use std::sync::Arc;
 
 use pnet::packet::icmp::echo_request::{MutableEchoRequestPacket, EchoRequestPacket};
 use pnet::packet::icmp::echo_reply::EchoReplyPacket;
@@ -12,23 +13,96 @@ use pnet::packet::Packet;
 
 use async_trait::async_trait;
 use tokio::io;
+use cs140_network::encoding::HandlePackage;
+use cs140_network::ip::{IPLayer, IPPackage};
+use crate::rpc::{CS120RPC, CS120Socket, IcmpPackage, Transport};
 
 pub struct IcmpSocket {
-    socket: Socket,
+    socket: Arc<Socket>,
 }
 
 impl IcmpSocket {
     pub fn new() -> Self {
-        let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4)).expect("couldn't not create a icmp socket");
+        let socket = Arc::new(Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4)).expect("couldn't not create a icmp socket"));
         IcmpSocket {
             socket
         }
     }
     pub(crate) async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> io::Result<usize> {
-        todo!()
+        let buf = buf.to_vec();
+        let addr: SockAddr = SockAddr::from(addr);
+        let socket = self.socket.clone();
+        tokio::spawn(async move {
+            socket.send_to(&buf,&addr)
+        }).await.unwrap()
     }
-    pub(crate) async fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        todo!()
+    pub(crate) async fn recv_from(&self) -> io::Result<(usize, SocketAddr, Vec<u8>)> {
+        let mut buf = Vec::with_capacity(65536);
+        buf.resize(65536,MaybeUninit::new(0));
+        let socket = self.socket.clone();
+        tokio::spawn(async move{
+            let result = socket.recv_from(buf.as_mut_slice());
+            result.map(|(size,addr)|{
+                let buf = buf.into_iter().map(|u|{
+                   unsafe{ u.assume_init()}
+                }).take(size).collect();
+                (size,addr.as_socket().unwrap(),buf)
+            })
+        }).await.unwrap()
+    }
+}
+
+pub struct AudioPinger {
+    layer: IPLayer,
+    sequence_number: u16,
+    identifier: u16
+}
+
+impl AudioPinger {
+    pub fn new(layer: IPLayer, identifier: u16) -> Self {
+        AudioPinger {
+            layer,
+            sequence_number: 0,
+            identifier,
+        }
+    }
+
+    pub async fn ping_once(&mut self, target: IpAddr) {
+        let src = SocketAddr::from(SocketAddrV4::from_str("192.168.1.2:1234").unwrap());
+        let dst = SocketAddr::new(target, 0);
+        let packet_size = EchoRequestPacket::minimum_packet_size();
+
+        let mut buf: Vec<u8> = vec![0; packet_size];
+        self.make_packet(&mut buf[..]);
+        let start_time = Instant::now();
+
+        self.layer.trans(CS120RPC::IcmpPackage(IcmpPackage{src, dst, types: IcmpTypes::EchoRequest.0, data: buf})).await;
+
+        self.sequence_number += 1;
+
+        if let CS120RPC::IcmpPackage(package) = self.layer.recv().await {
+            let buf = package.data.as_slice();
+            let addr = package.src;
+            if let Some(icmp_packet) = EchoReplyPacket::new(&buf[..]) {
+                println!("{:?}", icmp_packet);
+                println!("addr: {:?}", addr);
+                let duration = start_time.elapsed();
+                println!("time={}ms", duration.as_millis());
+            }
+        } else {
+            unreachable!()
+        }
+    }
+
+    fn make_packet(&self, buf: &mut [u8]) {
+        let mut echo_packet = MutableEchoRequestPacket::new(buf).unwrap();
+        echo_packet.set_sequence_number(self.sequence_number);
+        echo_packet.set_identifier(self.identifier);
+        echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
+        echo_packet.set_icmp_code(IcmpCode::new(0));
+
+        let echo_checksum = checksum(&IcmpPacket::new(echo_packet.packet()).unwrap());
+        echo_packet.set_checksum(echo_checksum);
     }
 }
 
@@ -45,36 +119,30 @@ impl Pinger {
         }
     }
 
-    pub fn run(&mut self, target: IpAddr, count: u32) {
-        let maybe_sock = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4));
+    async fn run(&mut self, target: IpAddr, count: u32) {
+        let sock = IcmpSocket::new();
 
-        if maybe_sock.is_err() {
-            return;
-        }
+        let dst_addr = SocketAddr::new(target, 0);
 
-        let sock = maybe_sock.unwrap();
-
-        let dst_addr = SockAddr::from(SocketAddr::new(target, 0));
         let packet_size = EchoRequestPacket::minimum_packet_size();
 
         let mut buf: Vec<u8> = vec![0; packet_size];
         for _ in 0..count {
             self.make_packet(&mut buf[..]);
             let start_time = Instant::now();
-            sock.send_to(&buf[..], &dst_addr).unwrap();
+            sock.send_to_addr(&buf[..], dst_addr).await.unwrap();
 
             buf.clear();
             buf.resize(packet_size, 0);
-            let mut buffer : Vec<MaybeUninit<u8>> = vec![MaybeUninit::new(0); 1024];
+            let mut buffer : Vec<u8> = vec![0; 1024];
 
-            let (len, address) = sock.recv_from(&mut buffer[..]).unwrap();
-
-            let buf:Vec<_> = buffer.iter().take(len).map(|x|unsafe{ x.assume_init() }).collect();
+            let (len, address) = sock.recv_from_addr(&mut buffer[..]).await.unwrap();
 
             self.sequence_number += 1;
 
             if let Some(icmp_packet) = EchoReplyPacket::new(&buf[..]) {
                 println!("{:?}", icmp_packet);
+                println!("addr: {:?}", address);
                 let duration = start_time.elapsed();
                 println!("time={}ms", duration.as_millis());
             }
@@ -96,9 +164,9 @@ impl Pinger {
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn ping_test() {
+    #[tokio::test]
+    async fn ping_test() {
         let mut ping = Pinger::new(0x0001);
-        ping.run(IpAddr::from_str("220.181.38.251").unwrap(), 20);
+        ping.run(IpAddr::from_str("220.181.38.251").unwrap(), 20).await;
     }
 }
