bb519cffed6d347403094075229e0ce9995f8fbd
ViXbob <vixbob@outlook.com>
2021-12-11 21:41:45 +0800

    fix bug

--- cs140-project3/src/redirect_server.rs
+++ cs140-project3/src/redirect_server.rs
@@ -18,17 +18,16 @@ async fn main() {
     loop {
         let result = icmp_socket.recv_from_addr(&mut buf).await;
         if let Ok((len, addr)) = result {
-            let mut data: Vec<_> = buf.iter().take(len).map(|x| *x).collect();
+            let mut data: Vec<_> = buf.iter().skip(20).take(len - 20).map(|x| *x).collect();
             let icmp_packet = MutableIcmpPacket::new(data.as_mut_slice()).unwrap();
             println!("icmp_packet: {:?}", icmp_packet);
             if icmp_packet.get_icmp_type() == IcmpTypes::EchoRequest {
-                let icmp_package = CS120RPC::IcmpPackage(IcmpPackage { src: addr, dst, types: IcmpTypes::EchoRequest.0, data: Vec::from(&data[..len]) });
+                let icmp_package = CS120RPC::IcmpPackage(IcmpPackage { src: addr, dst, types: IcmpTypes::EchoRequest.0, data });
                 println!("send: {:?}", icmp_package);
                 let encoded: Vec<u8> = bincode::encode_to_vec(icmp_package, Configuration::standard()).unwrap();
                 udp_socket.send_to(encoded.as_slice(), dst_addr).await;
             } else if icmp_packet.get_icmp_type() == IcmpTypes::EchoReply {
-                let data = &buf.clone().to_vec()[..len];
-                let mut decoded: CS120RPC = bincode::decode_from_slice(data, Configuration::standard()).unwrap();
+                let mut decoded: CS120RPC = bincode::decode_from_slice(data.as_slice(), Configuration::standard()).unwrap();
                 if let CS120RPC::IcmpPackage(package) = decoded {
                     icmp_socket.send_to_addr(package.data.as_slice(), package.dst).await;
                 } else {
