5b9fb68e35fe88386b225711ca27c8a94420ba64
ViXbob <vixbob@outlook.com>
2021-12-12 22:43:09 +0800

    fix icmp socket

--- cs140-util/src/icmp.rs
+++ cs140-util/src/icmp.rs
@@ -5,6 +5,7 @@ use std::net::{AddrParseError, IpAddr, SocketAddrV4};
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 use pnet::packet::icmp::echo_request::{MutableEchoRequestPacket, EchoRequestPacket, EchoRequest};
 use pnet::packet::icmp::echo_reply::{EchoReply, EchoReplyPacket, MutableEchoReplyPacket};
@@ -18,39 +19,68 @@ use cs140_network::ip::{IPLayer, IPPackage};
 use crate::rpc::{CS120RPC, CS120Socket, IcmpPackage, Transport};
 
 pub struct IcmpSocket {
-    socket: Arc<Socket>,
+    send_input_sender: Sender<(Vec<u8>,SocketAddr)>,
+    send_output_receiver: tokio::sync::Mutex<Receiver<io::Result<usize>>>,
+    recv_receiver: tokio::sync::Mutex<Receiver<io::Result<(usize, SocketAddr, Vec<u8>)>>>
 }
 
 impl IcmpSocket {
     pub fn new() -> Self {
         let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4)).expect("couldn't not create a icmp socket");
         // socket.set_read_timeout(Some(std::time::Duration::from_millis(100)));
-        let socket = Arc::new(socket);
+        let socket_for_send_to = Arc::new(socket);
+        let (send_input_sender, mut send_input_receiver) = channel::<(Vec<u8>, SocketAddr)>(1024);
+        let (send_output_sender,send_output_receiver) = channel(1024);
+        let (recv_sender,recv_receiver) = channel(1024);
+
+        let socket_for_recv = socket_for_send_to.clone();
+        tokio::spawn(async move{
+            loop{
+                let data = send_input_receiver.recv().await;
+                match data{
+                    None => {return}
+                    Some(data) => {
+                        let addr: SockAddr = SockAddr::from(data.1);
+                        let result = socket_for_send_to.send_to(&data.0,&addr);
+                        if send_output_sender.send(result).await.is_err(){
+                            return;
+                        }
+                    }
+                }
+            }
+        });
+
+        tokio::spawn(async move{
+            loop{
+                let mut buf = Vec::with_capacity(128);
+                buf.resize(128,MaybeUninit::new(0));
+                let result = socket_for_recv.recv_from(buf.as_mut_slice());
+                let result = result.map(|(size,addr)|{
+                        let buf = buf.into_iter().map(|u|{
+                            unsafe{ u.assume_init()}
+                        }).take(size).collect();
+                        (size,addr.as_socket().unwrap(),buf)
+                });
+                if recv_sender.send(result).await.is_err(){
+                    return;
+                }
+            }
+        });
+
         IcmpSocket {
-            socket
+            send_input_sender,
+            send_output_receiver:tokio::sync::Mutex::new(send_output_receiver),
+            recv_receiver: tokio::sync::Mutex::new(recv_receiver),
         }
     }
     pub(crate) async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> io::Result<usize> {
-        let buf = buf.to_vec();
-        let addr: SockAddr = SockAddr::from(addr);
-        let socket = self.socket.clone();
-        tokio::spawn(async move {
-            socket.send_to(&buf,&addr)
-        }).await.unwrap()
+        self.send_input_sender.send((buf.iter().cloned().collect(),addr)).await.unwrap();
+        let mut guard = self.send_output_receiver.lock().await;
+        guard.recv().await.unwrap()
     }
     pub(crate) async fn recv_from(&self) -> io::Result<(usize, SocketAddr, Vec<u8>)> {
-        let mut buf = Vec::with_capacity(128);
-        buf.resize(128,MaybeUninit::new(0));
-        let socket = self.socket.clone();
-        tokio::spawn(async move{
-            let result = socket.recv_from(buf.as_mut_slice());
-            result.map(|(size,addr)|{
-                let buf = buf.into_iter().map(|u|{
-                   unsafe{ u.assume_init()}
-                }).take(size).collect();
-                (size,addr.as_socket().unwrap(),buf)
-            })
-        }).await.unwrap()
+        let mut guard = self.recv_receiver.lock().await;
+        guard.recv().await.unwrap()
     }
 }
 
