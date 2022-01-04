89bc462c2d90435dcc68f01790095c83152779bc
ViXbob <vixbob@outlook.com>
2022-01-04 08:49:06 +0800

    test tcp in linux

--- cs140-project3/src/redirect_server_tcp.rs
+++ cs140-project3/src/redirect_server_tcp.rs
@@ -5,7 +5,7 @@ use cs140_util::tcp::tcp::TCPSocket;
 #[tokio::main]
 async fn main() {
     let mut tcp_socket = TCPSocket::new();
-    let buf: Vec<u8> = vec![43, 105, 0, 80, 253, 236, 248, 162, 0, 0, 0, 0, 128, 2, 0, 12, 218, 20, 0, 0, 2, 4, 0, 24, 3, 3, 0, 4, 2, 0, 0, 0]
+    let buf: Vec<u8> = vec![43, 105, 0, 80, 253, 236, 248, 162, 0, 0, 0, 0, 128, 2, 0, 12, 218, 20, 0, 0, 2, 4, 0, 24, 3, 3, 0, 4, 2, 0, 0, 0];
     let addr = Ipv4Addr::new(101, 32, 194, 18);
     let addr = SocketAddrV4::new(addr, 80);
     let addr = SocketAddr::from(addr);
