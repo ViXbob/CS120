68d8eaedf3f487a24637eb4fdce2e25c0aa869e3
ViXbob <vixbob@outlook.com>
2022-01-07 03:44:51 +0800

    add log

--- cs140-project3/src/new_nat_server.rs
+++ cs140-project3/src/new_nat_server.rs
@@ -3,6 +3,8 @@ use cs140_util::new_nat::run_nat_server;
 
 #[tokio::main]
 async fn main() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.format_timestamp_millis().init();
     run_nat_server(Ipv4Addr::new(10, 19, 73, 32), Ipv4Addr::new(10, 19, 75, 4)).await;
     std::thread::park();
 }
--- cs140-project3/src/new_redirect_server.rs
+++ cs140-project3/src/new_redirect_server.rs
@@ -3,5 +3,7 @@ use cs140_util::redirect::run_unix_redirect_server;
 
 #[tokio::main]
 async fn main() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.format_timestamp_millis().init();
     run_unix_redirect_server(Ipv4Addr::new(10, 19, 75, 4), Ipv4Addr::new(10, 19, 73, 32)).await;
 }
