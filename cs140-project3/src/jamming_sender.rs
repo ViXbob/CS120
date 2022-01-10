1f78946287ac260e63a1b6fe314967dda626a416
vixbob <vixbob@outlook.com>
2022-01-10 23:21:52 +0800

    upload sender

--- cs140-project3/src/jamming_sender.rs
+++ cs140-project3/src/jamming_sender.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+use cs140_network::encoding::HandlePackage;
 use cs140_network::ip::{IPLayer, IPPackage};
 use cs140_network::physical::PhysicalLayer;
 use cs140_network::redundancy::RedundancyLayer;
@@ -7,12 +10,15 @@ use cs140_network::tcp::TCPLayer;
 async fn main() {
     let mut builder = env_logger::Builder::from_default_env();
     builder.format_timestamp_millis().init();
-    let layer = PhysicalLayer::new(2,32);
+    let layer = PhysicalLayer::new(2,256);
     let layer = RedundancyLayer::new(layer);
     let layer = IPLayer::new(layer,1,2);
     let layer = TCPLayer::new(layer);
+    // let file = File::open(r"C:\Users\vixbo\Desktop\PackageInfo.txt").unwrap();
+    // let mut reader = BufReader::new(file);
+    // let mut string_to_send = String::new();
+    // reader.read_to_string(&mut string_to_send);
 
-    layer.send(&"123".to_string()).await;
-    let result:Option<String> = layer.receive().await;
-    assert_eq!(result,Some("123".to_string()));
+    layer.send_raw((0..=255).cycle().take(16384).collect()).await;
+    let _:String = layer.receive().await.unwrap();
 }
