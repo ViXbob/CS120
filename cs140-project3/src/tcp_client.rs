f80d3c2738c1fb81606fc542b28e9948eaa77994
vixbob <vixbob@outlook.com>
2022-01-12 15:24:48 +0800

    minor fix

--- cs140-project3/src/tcp_client.rs
+++ cs140-project3/src/tcp_client.rs
@@ -8,9 +8,9 @@ use cs140_util::tcp::tcp_stack::TCPClient;
 async fn main() {
     let mut builder = env_logger::Builder::from_default_env();
     builder.format_timestamp_millis().init();
-    let addr = std::net::Ipv4Addr::new(10, 20, 93, 103);
+    let addr = std::net::Ipv4Addr::new(10, 19, 73, 32);
     let mut tcp_socket = AthernetTcpSocket::new(1);
-    let src_port = 11113;
+    let src_port = 11116;
     tcp_socket.connect(addr, 18888, src_port).await;
     let mut data: Vec<u8> = Vec::new();
     loop {
